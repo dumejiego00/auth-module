8e38c4eefa17067a3857c64f97117bc42fe15ae0
Daniel <danumejiego@gmail.com>
2025-01-25 08:18:31 +0900

    databaseConnection.ts updated for hosting

--- controllers/databaseConnection.ts
+++ controllers/databaseConnection.ts
@@ -1,14 +1,25 @@
 import mysql, { Pool, PoolOptions } from "mysql2/promise";
 
-const dbConfigLocal: PoolOptions = {
-  host: process.env.DB_HOST as string,
-  user: process.env.DB_USER as string,
-  password: process.env.DB_PASSWORD as string,
-  database: process.env.DB_DATABASE as string,
-  multipleStatements: process.env.DB_MULTIPLE_STATEMENTS === "true",
-  namedPlaceholders: process.env.DB_NAMED_PLACEHOLDERS === "true",
+const isFreeDB = process.env.DB_ENV || false; 
+
+const dbConfigFreeDB: PoolOptions = {
+  host: process.env.DB_FREE_HOST as string,
+  user: process.env.DB_FREE_USER as string,
+  password: process.env.DB_FREE_PASSWORD as string,
+  database: process.env.DB_FREE_DATABASE as string,
+  multipleStatements: process.env.DB_FREE_MULTIPLE_STATEMENTS === "true",
+  namedPlaceholders: process.env.DB_FREE_NAMED_PLACEHOLDERS === "true",
 };
 
-const database: Pool = mysql.createPool(dbConfigLocal);
+const dbConfigLocal: PoolOptions = {
+  host: process.env.DB_LOCAL_HOST as string,
+  user: process.env.DB_LOCAL_USER as string,
+  password: process.env.DB_LOCAL_PASSWORD as string,
+  database: process.env.DB_LOCAL_DATABASE as string,
+  multipleStatements: process.env.DB_LOCAL_MULTIPLE_STATEMENTS === "true",
+  namedPlaceholders: process.env.DB_LOCAL_NAMED_PLACEHOLDERS === "true",
+};
+
+const database: Pool = mysql.createPool(isFreeDB ? dbConfigFreeDB : dbConfigLocal);
 
 export default database;
--- controllers/databaseTestConnection.ts
+++ controllers/databaseTestConnection.ts
@@ -8,12 +8,12 @@ let connection: Connection | null = null;
 export const getTestConnection = async (): Promise<Connection> => {
   if (!connection) {
     connection = await createConnection({
-      host: process.env.DB_HOST as string,
-      user: process.env.DB_USER as string,
-      password: process.env.DB_PASSWORD as string,
-      database: process.env.DB_TEST_DATABASE as string,
-      multipleStatements: process.env.DB_MULTIPLE_STATEMENTS === "true",
-      namedPlaceholders: process.env.DB_NAMED_PLACEHOLDERS === "true",
+      host: process.env.DB_LOCAL_HOST as string,
+      user: process.env.DB_LOCAL_USER as string,
+      password: process.env.DB_LOCAL_PASSWORD as string,
+      database: process.env.DB_LOCAL_TEST_DATABASE as string,
+      multipleStatements: process.env.DB_LOCAL_MULTIPLE_STATEMENTS === "true",
+      namedPlaceholders: process.env.DB_LOCAL_NAMED_PLACEHOLDERS === "true",
     });
   }
   return connection;
